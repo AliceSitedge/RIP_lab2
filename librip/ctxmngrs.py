f91d2b59d81f7d74dd8be0f5bc1a15f0dc4452f7
Alice Sitedge <a.seledkina@mail.ru>
2019-10-06 12:30:08 +0300

    deleted unnecessary context manager, added report

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -1,5 +1,4 @@
 import time
-from contextlib import contextmanager
 
 
 # Здесь необходимо реализовать
@@ -11,22 +10,9 @@ from contextlib import contextmanager
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
 class timer:
-    def __init__(self):
-        self.time = time.clock()
-
     def __enter__(self):
         self.time = time.clock()
         return self.time
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(time.clock() - self.time)
-
-
-@contextmanager
-def timer2():
-    begin = time.clock()
-    yield begin
-    try:
-        pass
-    finally:
-        print(time.clock() - begin)
